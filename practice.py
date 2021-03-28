d13432f6b6935d3f247a2ad18340bf6bcf62cc77
miraclelives <81082580+miraclelives@users.noreply.github.com>
2021-03-28 21:36:36 +0900

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,14 @@
+
+station="사당"
+print(station + "행 열차가 들어오고 있습니다.")
+#사당 대신에 신도림, 인천공항을 대신 넣으면 원하는 문장이 완성된다.
+
+
+
+
+
+
+from random import*
+number= int(randrange(4, 29))
+
+print("오프라인 스터디 모임 날짜는 매월 " + str(number) + "일로 선정되었습니다")
